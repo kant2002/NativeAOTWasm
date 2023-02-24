91ef1b78229d030232d14bb491ce45f4b523db9c
Andrii Kurdiumov <kant2002@gmail.com>
2023-02-24 12:18:09 +0600

    Add example of marshalling primitive types

--- hellojs/Program.cs
+++ hellojs/Program.cs
@@ -1,9 +1,26 @@
 ﻿using System.Runtime.InteropServices;
 
-Imports.test();
+Console.WriteLine($"Get following int from function: {Imports.get_int()}");
+Console.WriteLine($"Get following double from function: {Imports.get_double()}");
+//Console.WriteLine($"Get following string from function: {Imports.get_string()}");
+Imports.call_no_parameters();
+//Imports.alert("This is JS alert");
 
 static class Imports
 {
     [DllImport("*")]
-    public static extern void test();
+    public static extern void call_no_parameters();
+
+    [DllImport("*")]
+    public static extern void alert(string message);
+
+    // If JS returns double value, then it would be trimmed to nearest number with lowest absolute value.
+    [DllImport("*")]
+    public static extern int get_int();
+
+    [DllImport("*")]
+    public static extern double get_double();
+
+    [DllImport("*")]
+    public static extern string get_string();
 }
--- hellojs/js_exports.js
+++ hellojs/js_exports.js
@@ -1,5 +1,17 @@
 mergeInto(LibraryManager.library, {
-    test: function() {
-      alert('hi');
+    call_no_parameters: function() {
+      alert("This is a call without parameters");
     },
+    /*alert: function(message) {
+      alert(message);
+    },*/
+    get_int: function() {
+      return 42;
+    },
+    get_double: function() {
+      return 0.5;
+    },
+    /*get_string: function() {
+      return "This is string from JS";
+    },*/
 });
