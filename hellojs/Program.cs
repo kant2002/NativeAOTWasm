d55a6cdf534595957ec5dc6ec1d0fa5aec8404a6
Andrii Kurdiumov <kant2002@gmail.com>
2023-02-24 12:02:09 +0600

    Add sample how to call JS functions from C#

---
+++ hellojs/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+Imports.test();
+
+static class Imports
+{
+    [DllImport("*")]
+    public static extern void test();
+}
---
+++ hellojs/js_exports.js
@@ -0,0 +1,5 @@
+mergeInto(LibraryManager.library, {
+    test: function() {
+      alert('hi');
+    },
+});
