a82478067582a4bec381389c6b6dd43a541b3a74
Andrii Kurdiumov <kant2002@gmail.com>
2023-02-25 20:07:55 +0600

    Add hellofs proejct with example FS access For now it is only in-memory which is useless

---
+++ hellofs/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+Directory.CreateDirectory("test");
+Console.WriteLine("Database test created!");
+File.WriteAllText("test/myfile.txt", "This is some text");
+Console.WriteLine("File test/myfile.txt written!");
+var content = File.ReadAllText("test/myfile.txt");
+Console.WriteLine("File test/myfile.txt read!");
+Console.WriteLine(content);
