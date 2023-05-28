4a8d614535b4d49bcfb90704f83ef20f9d42a38b
Andrii Kurdiumov <kant2002@gmail.com>
2023-05-28 21:41:38 +0600

    Add sample for working with FS

--- hellowasi/Program.cs
+++ hellowasi/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, WASM!");
+﻿Console.WriteLine("Hello, WASM! Don't forget to add --mapdir .:.  when run wasmer");
+Directory.CreateDirectory("test");
+Console.WriteLine("Database test created!");
+File.WriteAllText("test/myfile.txt", "This is some text");
+Console.WriteLine("File test/myfile.txt written!");
+var content = File.ReadAllText("test/myfile.txt");
+Console.WriteLine("File test/myfile.txt read!");
+Console.WriteLine(content);
+Console.WriteLine("We are done here!");
